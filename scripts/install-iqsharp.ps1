21b00f2c43c810dfcb3d4391ac114f6d8f55c11f
Angela Burton <anburton@microsoft.com>
2020-05-11 17:50:12 -0700

    add more print statements

--- scripts/install-iqsharp.ps1
+++ scripts/install-iqsharp.ps1
@@ -22,14 +22,20 @@ if ($install) {
         dotnet tool install Microsoft.Quantum.IQSharp --version 0.11.2004.2825 --tool-path $Env:TOOLS_DIR
 
         $path = (Get-Item "$Env:TOOLS_DIR\dotnet-iqsharp*").FullName
-        & echo $path
         & $path install --user --path-to-tool $path
         Write-Host "iq# kernel installed ($LastExitCode)"
     } catch {
+        Write-Host ("====================================")
         Write-Host ("iq# installation threw error: " + $_)
         Write-Host ("iq# might not be correctly installed.")
         Write-Host ("exception: " + $_.Exception)
-        Write-Host ("error details: " + $_.ErrorDetails)
+        Write-Host ("error details: " + $_.Exception.Data)
+        Write-Host ("exception message: " + $_.Exception.Message)
+        Write-Host ("exception stack trace: " + $_.Exception.StackTrace)
+        Write-Host ("inner exception: " + $_.Exception.InnerException)
+        Write-Host ("inner exception message: " + $_.Exception.InnerException.Message)
+        Write-Host ("inner exception stack trace: " + $_.Exception.InnerException.StackTrace)
+        Write-Host ("====================================")
     }
 } else {
     Write-Host ("Microsoft.Quantum.IQSharp is already installed in this host.")
