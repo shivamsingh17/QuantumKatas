74a195ecc4acda15be5a267f611bd4327059071f
Angela Burton <anburton@microsoft.com>
2020-05-13 15:48:41 -0700

    try fix from conda build in iqsharp

--- scripts/install-iqsharp.ps1
+++ scripts/install-iqsharp.ps1
@@ -23,10 +23,8 @@ if ($install) {
 
     
         $path = (Get-Item "$Env:TOOLS_DIR\dotnet-iqsharp*").FullName
-        & $path install --user --path-to-tool $path --log-level "Debug" > output.txt 2>&1
+        & $path install --user --path-to-tool $path --log-level "Debug" 2>&1 | ForEach-Object { "$_" }
         Write-Host "iq# kernel installed ($LastExitCode)"
-        Write-Host "cat output.txt"
-        cat output.txt
     } catch {
         Write-Host ("iq# installation threw error: " + $_)
         Write-Host ("iq# might not be correctly installed.")
@@ -34,8 +32,6 @@ if ($install) {
 
         Write-Host("========================")
         Write-Host('Exit code: ' + $LastExitCode)
-        Write-Host "cat output.txt"
-        cat output.txt
         Write-Host("========================")
     }
 } else {
